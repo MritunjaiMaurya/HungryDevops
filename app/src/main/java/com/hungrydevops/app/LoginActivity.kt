284e39b370b3de8152469c379782e14907b86928
DC_47 <prateekrana10g@gmail.com>
2023-11-23 10:09:54 +0530

    commit by prateek splash & login & appicon

--- app/src/main/java/com/hungrydevops/app/LoginActivity.kt
+++ app/src/main/java/com/hungrydevops/app/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.hungrydevops.app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.hungrydevops.app.databinding.ActivityLoginBinding
@@ -11,5 +12,9 @@ class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.tvForgotPassword.setOnClickListener {
+            startActivity(Intent(this@LoginActivity,MainActivity::class.java))
+        }
     }
 }
